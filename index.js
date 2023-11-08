d61dbcea2dbb4e793fc10723ef2f3a0e5f9c3834
Alexandros-Gavriel <113684966+Alexandros-Gavriel@users.noreply.github.com>
2023-11-08 14:57:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,11 +45,51 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // function copyToClipboard(text) {
+    //     navigator.clipboard.writeText(text).then(() => {
+    //         console.log(`Copied to clipboard: ${text}`);
+    //     }).catch(err => {
+    //         console.error('Could not copy text: ', err);
+    //     });
+    // }
+
+    function copyToClipboardFallback(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+    
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+    
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+        } catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+    
+        document.body.removeChild(textArea);
+    }
+    
+
     function copyToClipboard(text) {
+        if (!navigator.clipboard) {
+            copyToClipboardFallback(text);
+            return;
+        }
         navigator.clipboard.writeText(text).then(() => {
             console.log(`Copied to clipboard: ${text}`);
         }).catch(err => {
             console.error('Could not copy text: ', err);
+            // Fallback if the Clipboard API is not available
+            copyToClipboardFallback(text);
         });
     }
+    
 });
